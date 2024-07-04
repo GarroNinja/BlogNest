2115eee2b8f54e46263eb979cb3bab3b18c73bb0
Garro <127025914+GarroNinja@users.noreply.github.com>
2024-07-04 20:54:20 +0530

    implemented template inheritence and control flow within html files

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Rithvik A",
+        "title": "Blog Post 1",
+        "content": "First Post Content",
+        "date_posted": "1 July, 2024"
+    },
+    {
+        "author": "Garro",
+        "title": "Blog Post 2",
+        "content": "Second Post Content",
+        "date_posted": "2 July, 2024"
+    }
+]
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
