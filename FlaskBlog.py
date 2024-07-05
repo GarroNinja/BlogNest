557665b7287c41caab564842e4aa10a6e3f9fc8b
Garro <127025914+GarroNinja@users.noreply.github.com>
2024-07-05 17:00:21 +0530

    fine tuning design

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 
 posts = [
     {
-        "author": "Rithvik A",
-        "title": "Blog Post 1",
-        "content": "First Post Content",
-        "date_posted": "1 July, 2024"
+        "author": "Marrow",
+        "title": "Rediscovering Nature: A Weekend Getaway in the Digital Age",
+        "content": "In our fast-paced, technology-driven lives, finding time to reconnect with nature can seem like a distant dream. Yet, spending a weekend immersed in the great outdoors can work wonders for our mental and physical well-being. Whether it’s a hike through a serene forest, a camping trip under the stars, or simply a day at a local park, nature offers a perfect escape from the digital grind. This past weekend, I traded my screen for scenic trails and found a refreshing sense of peace and clarity that only nature can provide. Disconnecting from devices to reconnect with the world around us isn’t just a break; it’s a vital reset for the soul.",
+        "date_posted": "July 1, 2024"
     },
     {
         "author": "Garro",
-        "title": "Blog Post 2",
-        "content": "Second Post Content",
-        "date_posted": "2 July, 2024"
+        "title": "Culinary Adventures: Exploring the World Through Food",
+        "content": "In our fast-paced, technology-driven lives, finding time to reconnect with nature can seem like a distant dream. Yet, spending a weekend immersed in the great outdoors can work wonders for our mental and physical well-being. Whether it’s a hike through a serene forest, a camping trip under the stars, or simply a day at a local park, nature offers a perfect escape from the digital grind. This past weekend, I traded my screen for scenic trails and found a refreshing sense of peace and clarity that only nature can provide. Disconnecting from devices to reconnect with the world around us isn’t just a break; it’s a vital reset for the soul.",
+        "date_posted": "July 2, 2024"
     }
 ]
 @app.route("/")
