af7e7358a02f40fc7e46536fefbb5a627898a4fe
Garro <127025914+GarroNinja@users.noreply.github.com>
2024-07-07 23:13:58 +0530

    restructured into a package

---
+++ flaskblog/__init__.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1e772b7b84f34b27b83f916e2d46a641'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
+db = SQLAlchemy(app)
+
+from flaskblog import routes
